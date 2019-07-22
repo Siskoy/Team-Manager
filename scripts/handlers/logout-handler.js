c65677487f9dc5a8308fad7df68efdb886d7e263
Siskoy <powerofblade@abv.bg>
2019-07-22 18:31:10 +0300

    Fixed logout incorrect redirect

--- scripts/handlers/logout-handler.js
+++ scripts/handlers/logout-handler.js
@@ -3,7 +3,9 @@ window.handler = window.handler || {};
 import { auth } from '../models/auth.js';
 
 handler.logout = function (ctx) {
-    auth.logout();
-    sessionStorage.clear();
-    ctx.redirect('/');
+    auth.logout()
+        .then(() => {
+            sessionStorage.clear();
+            ctx.redirect('/');
+        });
 }
