298af6969f78d1298e781f3d66111df24b455836
Siskoy <powerofblade@abv.bg>
2019-08-04 18:17:55 +0300

    Fixed join and leave team.

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,32 @@
 $(() => {
-    const app = Sammy('#main', async function () {
+    Sammy('#main', async function () {
         this.use('Handlebars', 'hbs');
 
         //Home
         this.get('/', homeController.getHome);
 
-        //About
+        //User
         this.get('#/about', homeController.getAbout);
 
-        //Register
         this.get('#/register', userController.getRegister)
         this.post('#/register', userController.postRegister)
 
-        //Login
         this.get('#/login', userController.getLogin);
         this.post('#/login', userController.postLogin);
 
-        //Logout
         this.get('#/logout', userController.logout);
 
         //Catalog-Team
         this.get('#/catalog', catalogController.getTeamCatalog);
         this.get('#/catalog/:id', catalogController.getTeamDetails);
+
         this.get('#/create', catalogController.getCreateTeam);
         this.post('#/create', catalogController.postCreateTeam);
-        this.get('#/join/:id', catalogController.joinTeam);
-        this.get('#/leave', catalogController.leaveTeam);
+
         this.get('#/edit/:id', catalogController.getEditInfo);
         this.post('#/edit/:id', catalogController.postEditInfo);
-    });
 
-    app.run('/')
+        this.get('#/join/:id', catalogController.joinTeam);
+        this.get('#/leave/:id', catalogController.leaveTeam);
+    }).run('/');
 });
--- scripts/models/teamsService.js
+++ scripts/models/teamsService.js
@@ -1,6 +1,5 @@
 let teamsService = (() => {
     function loadTeams() {
-        // Request teams from db
         return requester.get('appdata', 'teams', 'kinvey');
     }
 
@@ -28,46 +27,41 @@ let teamsService = (() => {
         return requester.post('appdata', 'teams', 'kinvey', teamData);
     }
 
-    function joinTeam(teamId) {
+    async function joinTeam(teamId) {
         let userData = {
             username: sessionStorage.getItem('username'),
             teamId: teamId
         };
 
+        await addMember(teamId);
         sessionStorage.setItem('teamId', teamId);
         return requester.update('user', sessionStorage.getItem('userId'), 'kinvey', userData);
     }
 
-    function leaveTeam() {
+    async function leaveTeam() {
         let userData = {
             username: sessionStorage.getItem('username'),
             teamId: ''
         };
 
+        await removeMember(sessionStorage.getItem('teamId'));
         sessionStorage.setItem('teamId', '');
         return requester.update('user', sessionStorage.getItem('userId'), userData, 'kinvey');
     }
 
-    function addMember(teamId) {
-        return fetch(`https://baas.kinvey.com/appdata/kid_S1iMzOQzH/teams/${teamId}`,
-            {
-                'method': 'GET',
-                'headers': {
-                    'Authorization': requester.makeAuth('kinvey')
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                data.members.push({ username: sessionStorage.getItem('username') });
-                fetch(`https://baas.kinvey.com/appdata/kid_S1iMzOQzH/teams/${teamId}`,
-                {
-                    'method': 'PUT',
-                    'headers': {
-                        'Authorization': 'Basic ' + btoa('Siskoy:1234')
-                    },
-                    'body': JSON.stringify(data)
-                })
-            });
+    async function addMember(teamId) {
+        let team = await loadTeamDetails(teamId);
+        team.members.push({ username: sessionStorage.getItem('username') });
+
+        return requester.update('appdata', 'teams/' + teamId, 'kinvey', team);
+    }
+
+    async function removeMember(teamId) {
+        let team = await loadTeamDetails(teamId);
+        const memberIndex = team.members.findIndex(x => x.username === sessionStorage.getItem('username'));
+        team.members.splice(memberIndex, 1);
+
+        return requester.update('appdata', 'teams/' + teamId, 'kinvey', team);
     }
 
     return {
